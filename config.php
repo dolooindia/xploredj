c6e70f5508928362f5fc03d0c956e1e047c15040
xploredj <doloo.india@gmail.com>
2012-02-01 19:23:05 +0530

    config

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'test');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rite123');
+define('DB_PASSWORD', 'test123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
 /** MySQL Database Name */
-define('DB_NAME', 'annualreport');
+define('DB_NAME', 'test');
 
 ?>
